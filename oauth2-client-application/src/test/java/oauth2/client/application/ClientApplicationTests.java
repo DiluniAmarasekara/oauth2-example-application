8e65ee61d3c58eca7169a5b987e88fe9a308b6ba
DiluniAmarasekara <dilunia6@gmail.com>
2020-08-31 23:18:31 +0530

    initial commit

---
+++ oauth2-client-application/src/test/java/oauth2/client/application/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package oauth2.client.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ oauth2-resource-application/src/test/java/oauth2/resource/application/ResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package oauth2.resource.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
